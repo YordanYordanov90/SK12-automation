2fd285a4834c331c8e9921d5ba9f9130e1b703c7
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-02-15 21:45:37 +0200

    new

--- src/Lecture4/Car.java
+++ src/Lecture4/Car.java
@@ -2,15 +2,47 @@ package Lecture4;
 
 public class Car {
 
-    String name;
-    String color;
-    int releseYear;
-    int horsePower;
-    boolean secondHand;
+    public String name ;
+   public  String color = "N/A";
+    public int releaseYear = -1;
+    public int horsePower = -1;
+   public boolean secondHand = false;
 
-    public Car(String name, String ) {
+    public Car(String name, String color, int releaseYear, int horsePower, boolean secondHand) {
+
+       this.name = name;
+       this.color = color;
+       this.releaseYear = releaseYear;
+       this.horsePower = horsePower;
+       this.secondHand = secondHand;
 
     }
+
+    public Car(String name, String color, boolean secondHand){
+
+        this.name = name;
+        this.color = color;
+        this.secondHand = secondHand;
+
+    }
+
+    public Car(String name, String color, int releaseYear, int horsePower){
+
+        this.name = name;
+        this.color = color;
+        this.releaseYear = releaseYear;
+        this.horsePower = horsePower;
+
+    }
+
+    public Car(String name, int releaseYear, int horsePower, boolean secondHand) {
+        this(name,"N/A", releaseYear, horsePower, secondHand);
+    }
+
+    public Car(){
+
+    }
+
 
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Lecture4.Car;
+
 public class Main {
     public static void main(String[] args) {
-     int n = 3;
-        System.out.println(n);
+
+        Car car1 = new Car("Ford", "Red", 2015, 100, true);
+        Car car2 = new Car("N/A", "Blue", -1, -1, true);
+        Car car3 = new Car("BMW", "Black", 2010, 200);
+        Car car4 = new Car("Opel", "N/A", 2017,125, false);
+        Car car5 = new Car();
 
 
+        System.out.println(car1.name);
+        System.out.println(car2.color);
     }
 }
