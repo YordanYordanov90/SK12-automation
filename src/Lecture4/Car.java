7b768af19bdc3fc2d52de93d008d233610b40498
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-02-15 20:41:11 +0200

    new

---
+++ src/Lecture4/Car.java
@@ -0,0 +1,16 @@
+package Lecture4;
+
+public class Car {
+
+    String name;
+    String color;
+    int releseYear;
+    int horsePower;
+    boolean secondHand;
+
+    public Car(String name, String ) {
+
+    }
+
+}
+
